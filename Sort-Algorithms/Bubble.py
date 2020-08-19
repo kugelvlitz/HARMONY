8ce0d6ec58bdc4626ce82b55129317e2308d4285
David Azofeifa <david.azoherra@gmail.com>
2020-08-19 15:45:03 -0600

    updated examples so they dont run if they're not the file being directly executed

--- Sort-Algorithms/Bubble.py
+++ Sort-Algorithms/Bubble.py
@@ -8,5 +8,6 @@ def bubble_sort(arr: list) -> list:
                 sorted = False
     return arr
 
-# Example
-print(bubble_sort([4, 8, 6, 71, 52, 51, 11, 73, 2, 62, 88, 29, 5, 61, 31, 40, 30, 94, 89, 100]))
+if __name__ == '__main__':
+    # Example
+    print(bubble_sort([4, 8, 6, 71, 52, 51, 11, 73, 2, 62, 88, 29, 5, 61, 31, 40, 30, 94, 89, 100]))
--- Sort-Algorithms/insertion.py
+++ Sort-Algorithms/insertion.py
@@ -1,12 +1,13 @@
 def insertion(arr: list) -> list:
-    for i in range(len(arr)):
-        if i !=0 and arr[i] < arr[i-1]:
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
             for k in range(i, 0, -1):
                 if arr[k] < arr[k-1]:
                     arr[k], arr[k-1] = arr[k-1], arr[k]
     return arr
 
 
-# Example
-print(insertion([6,4,8,2,4,9]))
-print(insertion([23, 61, 55, 31, 12, 39, 40, 56, 72, 18, 4, 54, 30, 76, 20, 96, 77, 48, 70, 13]))
+if __name__ == '__main__':
+    # Example
+    print(insertion([6,4,8,2,4,9]))
+    print(insertion([23, 61, 55, 31, 12, 39, 40, 56, 72, 18, 4, 54, 30, 76, 20, 96, 77, 48, 70, 13]))
